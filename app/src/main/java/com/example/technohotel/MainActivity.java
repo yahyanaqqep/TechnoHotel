a0a45752a8a10d1b1ab46b55b2fa137acf82fb5b
Yahya Alnaqqep <104467283+yahyanaqqep@users.noreply.github.com>
2022-08-23 21:06:09 +0200

    latest update 23/8

--- app/src/main/java/com/example/technohotel/MainActivity.java
+++ app/src/main/java/com/example/technohotel/MainActivity.java
@@ -1,8 +1,16 @@
 package com.example.technohotel;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+
+import androidx.drawerlayout.widget.DrawerLayout;
+import android.os.Bundle;
+import android.view.MenuItem;
 
 import android.os.Bundle;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        DrawerLayout = findViewById(R.id.my_drawer_layout);
+        ActionBarDrawerToggle = new ActionBarDrawerToggle(this, DrawerLayout, R.string.nav_open, R.string.nav_close);
+
+        // pass the Open and Close toggle for the drawer layout listener
+        // to toggle the button
+        DrawerLayout.addDrawerListener(ActionBarDrawerToggle);
+        ActionBarDrawerToggle.syncState();
+
+        // to make the Navigation drawer icon always appear on the action bar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
